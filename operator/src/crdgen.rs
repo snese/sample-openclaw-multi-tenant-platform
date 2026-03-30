697a962eeb6cf31aa4cdfbdfb3ef94e658d27a43
Hung-Che Lo <hclo@snese.net>
2026-03-31 01:19:49 +0800

    refactor: split controller.rs into types, resources, and controller modules (#84)

--- operator/src/crdgen.rs
+++ operator/src/crdgen.rs
@@ -1,5 +1,5 @@
 use kube::CustomResourceExt;
-use tenant_operator::controller::Tenant;
+use tenant_operator::Tenant;
 
 fn main() {
     print!("{}", serde_yaml::to_string(&Tenant::crd()).unwrap());
